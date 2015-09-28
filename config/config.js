51f93b486ea28923b3127190bb20f82a16ed5deb
skyees <inforajkiran@gmail.com>
2015-09-28 13:54:52 +0530

    make it better

--- config/config.js
+++ config/config.js
@@ -27,6 +27,7 @@ module.exports.getGlobbedFiles = function(globPatterns, removeRoot) {
 	// The output array
 	var output = [];
 
+
 	// If glob pattern is array so we use each pattern in a recursive way, otherwise we use glob 
 	if (_.isArray(globPatterns)) {
 		globPatterns.forEach(function(globPattern) {
