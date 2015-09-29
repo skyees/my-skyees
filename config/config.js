b8fa4b4ff6537d03544f37379b139c2d7e382cb2
skyees <inforajkiran@gmail.com>
2015-09-29 10:17:06 +0530

    make it better

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@
  */
 var _ = require('lodash'),
 	glob = require('glob');
-
+var output = [];
 /**
  * Load app configurations
  */
@@ -18,36 +18,34 @@ module.exports = _.extend(
  * Get files by glob patterns
  */
 module.exports.getGlobbedFiles = function(globPatterns, removeRoot) {
-	// For context switching
-	var _this = this;
+    // For context switching
+    var _this = this;
 
-	// URL paths regex
-	var urlRegex = new RegExp('^(?:[a-z]+:)?\/\/', 'i');
+// URL paths regex
+    var urlRegex = new RegExp('^(?:[a-z]+:)?\/\/', 'i');
 
-	// The output array
-	var output = [];
-
-
-	// If glob pattern is array so we use each pattern in a recursive way, otherwise we use glob 
-	if (_.isArray(globPatterns)) {
-		globPatterns.forEach(function(globPattern) {
-			output = _.union(output, _this.getGlobbedFiles(globPattern, removeRoot));
-		});
-	} else if (_.isString(globPatterns)) {
-		if (urlRegex.test(globPatterns)) {
-			output.push(globPatterns);
-		}   else {
-            var files = glob(globPatterns, { sync: true });
-            if (removeRoot) {
-                files = files.map(function(file) {
-                    return file.replace(removeRoot, '');
-                });
-            }
-            output = _.union(output, files);
+// If glob pattern is array so we use each pattern in a recursive way, otherwise we use glob
+    if (_.isArray(globPatterns)) {
+        globPatterns.forEach(function(globPattern) {
+            output = _.union(output, _this.getGlobbedFiles(globPattern, removeRoot));
+        });
+    } else if (_.isString(globPatterns)) {
+        if (urlRegex.test(globPatterns)) {
+            output.push(globPatterns);
+        } else {
+            glob(globPatterns, { sync: true }, function(err, files) {
+                if (removeRoot) {
+                    files = files.map(function(file) {
+                        return file.replace(removeRoot, '');
+                    });
+                }
+                output = _.union(output, files);
+            });
         }
-	}
+    }
 
-	return output;
+// return the array of globbed files
+    return output;
 };
 
 /**
